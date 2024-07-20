dd5a881f9ef3a0e76d22e0af5eec467b80c6259f
Diego Palomar <dfpalomar@gmail.com>
2024-07-20 09:10:18 +0100

    Testing endpoints

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,20 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, TypeScript with Express!");
+  return res.status(200).json({
+    name: "Diego",
+    surname: "Palomar",
+  });
+});
+
+app.get("/faq", (req, res) => {
+  const userQuestion = req.query.q;
+
+  if (!userQuestion) {
+    return res.status(400).json({ error: "Question is required" });
+  } else {
+    res.send("Your question is: " + userQuestion);
+  }
 });
 
 app.listen(port, () => {
